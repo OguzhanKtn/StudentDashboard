5e719e6169c6ebf6b1d24e2c91bf5a1600fbccdc
Oguzhan Kotan <81297977+OguzhanKtn@users.noreply.github.com>
2023-10-30 13:56:07 +0300

    Update ApplicationController.java

--- Spring/src/main/java/com/works/restcontrollers/ApplicationController.java
+++ Spring/src/main/java/com/works/restcontrollers/ApplicationController.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/application")
+@CrossOrigin
 public class ApplicationController {
 
     final ApplicationService applicationService;
