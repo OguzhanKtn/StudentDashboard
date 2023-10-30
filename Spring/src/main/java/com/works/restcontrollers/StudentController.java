3fd16817867714c4fdcbe87d0892a51ce6ab065f
Oguzhan Kotan <81297977+OguzhanKtn@users.noreply.github.com>
2023-10-30 13:56:25 +0300

    Update StudentController.java

--- Spring/src/main/java/com/works/restcontrollers/StudentController.java
+++ Spring/src/main/java/com/works/restcontrollers/StudentController.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/student")
+@CrossOrigin
 public class StudentController {
 
     final StudentService studentService;
